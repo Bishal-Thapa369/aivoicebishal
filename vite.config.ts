0a1a835322250dd538e4eaf6ae00592754a3d47a
Bishal Thapa <nitrokali@kali.nitro.kali.com>
2025-08-15 01:55:25 +0545

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // This is the only configuration you need for GitHub Pages.
+  // It must match the name of your repository.
+  base: '/aivoicebishal/',
+});
